81cc4133b6848ef9d99df2645180b85f6b6d3383
Leonardo Grzybowski <leonardogrz@hotmail.com>
2023-06-27 14:12:31 -0300

    fix: still fixing more lint errors in script.js

--- js/script.js
+++ js/script.js
@@ -8,18 +8,16 @@ btnLogin.addEventListener('click', (e) => {
   e.preventDefault();
   if (emailForm.value === 'tryber@teste.com' && passwordForm.value === '123456') {
     alert('Olá, Tryber!');
-  }
-  else {
+  } else {
     alert('Email ou senha inválidos.');
   }
 });
 
 textComments.addEventListener('keyup', () => {
-  charCount.innerText = eval(500 - textComments.value.length);
+  charCount.innerText = (500 - textComments.value.length);
   if (charCount.innerText <= parseInt(20)) {
     charCount.style.color = 'red';
-  }
-  else {
+  } else {
     charCount.style.color = 'black';
   }
 });
