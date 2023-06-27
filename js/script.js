9e6cbb23229c931e409143a50b1ca10a922c478a
Leonardo Grzybowski <leonardogrz@hotmail.com>
2023-06-27 11:26:43 -0300

    feat: script.js created | login handler created | char counter created

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const btnLogin = document.getElementById('btnLogin');
+const emailForm = document.getElementById('emailForm');
+const passwordForm = document.getElementById('passwordForm');
+const charCount = document.getElementById('counter');
+const textComments = document.getElementById('textarea');
+
+
+btnLogin.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (emailForm.value === 'tryber@teste.com' && passwordForm.value === '123456') {
+    alert('Olá, Tryber!');
+  } 
+  else {
+    alert('Email ou senha inválidos.');
+  }
+})
+
+textComments.addEventListener('keyup', () => {
+  charCount.innerText = eval(500 - textComments.value.length);
+  if (charCount.innerText <= parseInt(20)) {
+    charCount.style.color = 'red';
+  }
+  else {
+    charCount.style.color = 'black';
+  }
+
+})
