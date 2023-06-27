a5ada42d4ab2201c3790fddac11946a26538decb
Leonardo Grzybowski <leonardogrz@hotmail.com>
2023-06-27 18:06:59 -0300

    fix: fixing js bugs | works on npm test but doesn't work on github

--- js/script.js
+++ js/script.js
@@ -1,33 +1,36 @@
 const btnLogin = document.getElementById('btnLogin');
 const emailForm = document.getElementById('emailForm');
 const passwordForm = document.getElementById('passwordForm');
+const agreement = document.getElementById('agreement');
+const submitBtn = document.getElementById('submit-btn');
 const charCount = document.getElementById('counter');
 const textComments = document.getElementById('textarea');
-const btnSubmit = document.getElementById('submit-btn');
-const checkBox = document.getElementById('agreement');
 
 btnLogin.addEventListener('click', (e) => {
   e.preventDefault();
   if (emailForm.value === 'tryber@teste.com' && passwordForm.value === '123456') {
     alert('Olá, Tryber!');
-  } else {
+  }
+  else {
     alert('Email ou senha inválidos.');
   }
 });
 
-textComments.addEventListener('keyup', () => {
-  charCount.innerText = (500 - textComments.value.length);
-  if (Number(charCount.innerText) <= 20) {
-    charCount.style.color = 'red';
-  } else {
-    charCount.style.color = 'black';
+agreement.addEventListener('click', () => {
+  if (agreement.checked) {
+    submitBtn.disabled = false;
+  }
+  else {
+    submitBtn.disabled = true;
   }
 });
 
-checkBox.addEventListener('click', () => {
-  if (checkBox.checked === false) {
-    btnSubmit.disabled = true;
-  } else {
-    btnSubmit.disabled = false;
+textComments.addEventListener('keyup', () => {
+  charCount.innerText = eval(500 - textComments.value.length);
+  if (charCount.innerText <= parseInt(20)) {
+    charCount.style.color = 'red';
+  }
+  else {
+    charCount.style.color = 'black';
   }
 });
